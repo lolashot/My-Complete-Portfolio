1841ff2daaf4b1256af12ec5b403bdaf7bafd7f7
lolashot <ololadeshot@gmail.com>
2022-09-05 22:01:06 +0100

    added hamburger toggle and styling

---
+++ scripts/header.js
@@ -0,0 +1,7 @@
+
+function toggleMenu() {
+    const hamburger = document.querySelector('.hamburger');
+    const navigate = document.querySelector('.navigate');
+    hamburger.classList.toggle('active');
+    navigate.classList.toggle('active');
+}
