7a80a09ab8e7aa41299fc93177a9670b2800d0e7
lolashot <ololadeshot@gmail.com>
2022-09-06 20:22:38 +0100

    review changes

--- header.js
+++ header.js
@@ -2,8 +2,12 @@ const hamburger = document.querySelector('#clicktoggle');
 hamburger.addEventListener('click', () => {
   const hamburger = document.querySelector('.hamburger');
   const navigate = document.querySelector('.navigate');
+  const hero = document.querySelector('.hero');
+  const headerBrand = document.querySelector('.headerBrand');
   hamburger.classList.toggle('active');
   navigate.classList.toggle('active');
+  hero.classList.toggle('greyed_out');
+  headerBrand.classList.toggle('greyed_out');
 });
 const toggle = document.querySelector('#toggle');
 toggle.addEventListener('click', () => {
