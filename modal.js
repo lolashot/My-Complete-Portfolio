2478c5c3dd1bdc17df47a719c1781c2858d9c2fd
lolashot <ololadeshot@gmail.com>
2022-09-08 06:53:17 +0100

    check

--- modal.js
+++ modal.js
@@ -1,41 +1,41 @@
-const projects = [
-  {
-    id: 1,
-    name: 'React Reserve',
-    technologies: 'MERN Stack',
-    description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-    featuredimage: './project-1.gif',
-    linktoliveserver: 'https://reactbootcamp.com',
-    linktosource: 'https://reactbootcamp.com',
-  },
-  {
-    id: 2,
-    name: 'Js Reserve',
-    technologies: 'Js Stack',
-    description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-    featuredimage: './project-1.gif',
-    linktoliveserver: 'https://reactbootcamp.com',
-    linktosource: 'https://reactbootcamp.com',
-  },
-  {
-    id: 3,
-    name: 'html Reserve',
-    technologies: 'MERN Stack',
-    description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-    featuredimage: './project-1.gif',
-    linktoliveserver: 'https://reactbootcamp.com',
-    linktosource: 'https://reactbootcamp.com',
-  },
-  {
-    id: 4,
-    name: 'css Reserve',
-    technologies: 'css Stack',
-    description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-    featuredimage: './project-1.gif',
-    linktoliveserver: 'https://reactbootcamp.com',
-    linktosource: 'https://reactbootcamp.com',
-  },
-];
+// const projects = [
+//   {
+//     id: 1,
+//     name: 'React Reserve',
+//     technologies: 'MERN Stack',
+//     description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+//     featuredimage: './project-1.gif',
+//     linktoliveserver: 'https://reactbootcamp.com',
+//     linktosource: 'https://reactbootcamp.com',
+//   },
+//   {
+//     id: 2,
+//     name: 'Js Reserve',
+//     technologies: 'Js Stack',
+//     description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+//     featuredimage: './project-1.gif',
+//     linktoliveserver: 'https://reactbootcamp.com',
+//     linktosource: 'https://reactbootcamp.com',
+//   },
+//   {
+//     id: 3,
+//     name: 'html Reserve',
+//     technologies: 'MERN Stack',
+//     description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+//     featuredimage: './project-1.gif',
+//     linktoliveserver: 'https://reactbootcamp.com',
+//     linktosource: 'https://reactbootcamp.com',
+//   },
+//   {
+//     id: 4,
+//     name: 'css Reserve',
+//     technologies: 'css Stack',
+//     description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+//     featuredimage: './project-1.gif',
+//     linktoliveserver: 'https://reactbootcamp.com',
+//     linktosource: 'https://reactbootcamp.com',
+//   },
+// ];
 // const closeBtn = document.getElementById('close-btn');
 // const modalContainer = document.getElementById('modal-container');
 // function assignValue(itemId) {
