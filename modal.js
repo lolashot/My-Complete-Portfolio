141f3f7979acb450356635d43d2af7e62ea31849
lolashot <ololadeshot@gmail.com>
2022-09-08 07:00:02 +0100

    check

--- modal.js
+++ modal.js
@@ -3,7 +3,8 @@
 //     id: 1,
 //     name: 'React Reserve',
 //     technologies: 'MERN Stack',
-//     description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+//     description: 'rem ipsum dolor sit amet consectetur adipisicing elit.
+//       Praesentium dolore rerum laborum iureenim sint nemo omnis voluptate exercitationem eius?',
 //     featuredimage: './project-1.gif',
 //     linktoliveserver: 'https://reactbootcamp.com',
 //     linktosource: 'https://reactbootcamp.com',
@@ -12,7 +13,8 @@
 //     id: 2,
 //     name: 'Js Reserve',
 //     technologies: 'Js Stack',
-//     description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+//     description: 'rem ipsum dolor sit amet consectetur adipisicing elit.
+//       Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
 //     featuredimage: './project-1.gif',
 //     linktoliveserver: 'https://reactbootcamp.com',
 //     linktosource: 'https://reactbootcamp.com',
@@ -21,7 +23,8 @@
 //     id: 3,
 //     name: 'html Reserve',
 //     technologies: 'MERN Stack',
-//     description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+//     description: 'rem ipsum dolor sit amet consectetur adipisicing elit.
+//       Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
 //     featuredimage: './project-1.gif',
 //     linktoliveserver: 'https://reactbootcamp.com',
 //     linktosource: 'https://reactbootcamp.com',
@@ -30,7 +33,8 @@
 //     id: 4,
 //     name: 'css Reserve',
 //     technologies: 'css Stack',
-//     description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+//     description: 'rem ipsum dolor sit amet consectetur adipisicing elit.
+//       Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
 //     featuredimage: './project-1.gif',
 //     linktoliveserver: 'https://reactbootcamp.com',
 //     linktosource: 'https://reactbootcamp.com',
