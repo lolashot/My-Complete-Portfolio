f2cf3b8f82ff82b23b1447ae5ec62365c6717ac9
lolashot <ololadeshot@gmail.com>
2022-09-08 10:19:20 +0100

    linter correction

--- modal.js
+++ modal.js
@@ -1,58 +1,54 @@
-/* eslint-disable no-console */
 const projects = [
   {
-      id: 1,
-      name: 'React Reserve',
-      technologies: 'MERN Stack',
-      description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-      featuredimage: './project-1.gif',
-      linktoliveserver: 'https://reactbootcamp.com',
-      linktosource: 'https://reactbootcamp.com',
-    },
-    {
-      id: 2,
-      name: 'Js Reserve',
-      technologies: 'Js Stack',
-      description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-      featuredimage: './project-1.gif',
-      linktoliveserver: 'https://reactbootcamp.com',
-      linktosource: 'https://reactbootcamp.com',
-    },
-    {
-      id: 3,
-      name: 'html Reserve',
-      technologies: 'MERN Stack',
-      description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-      featuredimage: './project-1.gif',
-      linktoliveserver: 'https://reactbootcamp.com',
-      linktosource: 'https://reactbootcamp.com',
-    },
-    {
-      id: 4,
-      name: 'css Reserve',
-      technologies: 'css Stack',
-      description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-      featuredimage: './project-1.gif',
-      linktoliveserver: 'https://reactbootcamp.com',
-      linktosource: 'https://reactbootcamp.com',
-    },
-  
-  ];
-  
-// import projects from './data.js';
+    id: 1,
+    name: 'React Reserve',
+    technologies: 'MERN Stack',
+    description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    featuredimage: './project-1.gif',
+    linktoliveserver: 'https://reactbootcamp.com',
+    linktosource: 'https://reactbootcamp.com',
+  },
+  {
+    id: 2,
+    name: 'Js Reserve',
+    technologies: 'Js Stack',
+    description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    featuredimage: './project-1.gif',
+    linktoliveserver: 'https://reactbootcamp.com',
+    linktosource: 'https://reactbootcamp.com',
+  },
+  {
+    id: 3,
+    name: 'html Reserve',
+    technologies: 'MERN Stack',
+    description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    featuredimage: './project-1.gif',
+    linktoliveserver: 'https://reactbootcamp.com',
+    linktosource: 'https://reactbootcamp.com',
+  },
+  {
+    id: 4,
+    name: 'css Reserve',
+    technologies: 'css Stack',
+    description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
+    featuredimage: './project-1.gif',
+    linktoliveserver: 'https://reactbootcamp.com',
+    linktosource: 'https://reactbootcamp.com',
+  },
+];
+
 const closeBtn = document.getElementById('close-btn');
 const modalContainer = document.getElementById('modal-container');
-
 function assignValue(itemId) {
-   const found = projects.find((element) => element.id === itemId);
-   console.log(found);
-   document.getElementById('greeting').innerHTML = found.name;
-   document.getElementById('say').innerHTML = found.technologies;
-   document.getElementById('hey').innerHTML = found.description;
-   document.getElementById('hi').innerHTML = found.featuredimage;
-   document.getElementById('sayy').innerHTML = found.linktoliveserver;
-   document.getElementById('sayyy').innerHTML = found.linktosource;
-   modalContainer.style.display = 'block';
+  const found = projects.find((element) => element.id === itemId);
+  console.log(found);
+  document.getElementById('greeting').innerHTML = found.name;
+  document.getElementById('say').innerHTML = found.technologies;
+  document.getElementById('hey').innerHTML = found.description;
+  document.getElementById('hi').innerHTML = found.featuredimage;
+  document.getElementById('sayy').innerHTML = found.linktoliveserver;
+  document.getElementById('sayyy').innerHTML = found.linktosource;
+  modalContainer.style.display = 'block';
 }
 
 closeBtn.addEventListener('click', () => {
