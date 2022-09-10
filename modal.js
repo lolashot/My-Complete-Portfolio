d374d3213a3c9bffe50a214e35768bd7d6e7cbd8
lolashot <ololadeshot@gmail.com>
2022-09-10 14:35:50 +0100

    mobile version responsiveness done

--- modal.js
+++ modal.js
@@ -1,39 +1,47 @@
 const projects = [
   {
     id: 1,
-    name: 'React Reserve',
-    technologies: 'MERN Stack',
+    name: 'task tracker',
+    technologies: 'html',
+    technologiess: 'css',
+    technologiesss: 'javaScript',
     description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-    featuredimage: './project-1.gif',
-    linktoliveserver: 'https://reactbootcamp.com',
-    linktosource: 'https://reactbootcamp.com',
+    featuredimage: './assets/desktop-images/SnapshootPortfolio.png',
+    linktoliveserver: 'https://lolashot.github.io/My-Complete-Portfolio',
+    linktosource: 'https://github.com/lolashot/My-Complete-Portfolio/',
   },
   {
     id: 2,
     name: 'Js Reserve',
-    technologies: 'Js Stack',
+    technologies: 'html',
+    technologiess: 'css',
+    technologiesss: 'javaScript',
     description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-    featuredimage: './project-1.gif',
-    linktoliveserver: 'https://reactbootcamp.com',
-    linktosource: 'https://reactbootcamp.com',
+    featuredimage: './assets/desktop-images/SnapshootPortfolio1.png',
+    linktoliveserver: 'https://lolashot.github.io/My-Complete-Portfolio',
+    linktosource: 'https://github.com/lolashot/My-Complete-Portfolio/',
   },
   {
     id: 3,
     name: 'html Reserve',
-    technologies: 'MERN Stack',
+    technologies: 'html',
+    technologiess: 'css',
+    technologiesss: 'javaScript',
     description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-    featuredimage: './project-1.gif',
-    linktoliveserver: 'https://reactbootcamp.com',
-    linktosource: 'https://reactbootcamp.com',
+    featuredimage: './assets/desktop-images/SnapshootPortfolio2.png',
+    linktoliveserver: 'https://lolashot.github.io/My-Complete-Portfolio',
+    linktosource: 'https://github.com/lolashot/My-Complete-Portfolio/',
   },
   {
     id: 4,
     name: 'css Reserve',
-    technologies: 'css Stack',
+    technologies: 'html',
+    technologiess: 'css',
+    technologiesss: 'javaScript',
     description: 'rem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-    featuredimage: './project-1.gif',
-    linktoliveserver: 'https://reactbootcamp.com',
-    linktosource: 'https://reactbootcamp.com',
+    featuredimage: './assets/desktop-images/SnapshootPortfolio3.png',
+    linktoliveserver: 'https://lolashot.github.io/My-Complete-Portfolio',
+    linktosource: 'https://github.com/lolashot/My-Complete-Portfolio/',
   },
 ];
 
@@ -41,13 +49,18 @@ const closeBtn = document.getElementById('close-btn');
 const modalContainer = document.getElementById('modal-container');
 function assignValue(itemId) {
   const found = projects.find((element) => element.id === itemId);
+  // const a = document.getElementById('live');
+
   console.log(found);
-  document.getElementById('greeting').innerHTML = found.name;
-  document.getElementById('say').innerHTML = found.technologies;
-  document.getElementById('hey').innerHTML = found.description;
-  document.getElementById('hi').innerHTML = found.featuredimage;
-  document.getElementById('sayy').innerHTML = found.linktoliveserver;
-  document.getElementById('sayyy').innerHTML = found.linktosource;
+  document.getElementById('name').innerHTML = found.name;
+  document.getElementById('technology1').innerHTML = found.technologies;
+  document.getElementById('technology2').innerHTML = found.technologiess;
+  document.getElementById('technology3').innerHTML = found.technologiesss;
+  document.getElementById('description').innerHTML = found.description;
+  document.getElementById('image').src = found.featuredimage;
+  document.getElementById('live').href = found.linktoliveserver;
+  // document.getElementById('live').style.fontSize = '1px';
+  document.getElementById('source').href = found.linktosource;
   modalContainer.style.display = 'block';
 }
 
